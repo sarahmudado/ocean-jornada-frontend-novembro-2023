f3073666e94aff9bd1516efa645ef9e6e57c56d3
sarahmudado <150301556+sarahmudado@users.noreply.github.com>
2023-11-09 01:11:44 +0000

    Criação card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
 import './App.css'
 
 function App() {
   return (
     <>
-     
+    <div> 
+      <h2>Rick Sanches</h2>
+      <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+        </div>
     </>
   )
 }
